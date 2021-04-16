f9aa70e2a0f8d0754de70711a306cac014159f4b
Matthias <matthias.maszuhn@uni-oldenburg.de>
2021-04-16 23:08:10 +0200

    Fog

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
