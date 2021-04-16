a6995857b0be3c3bd035fb0c578c6faa6aae0164
Christian Förster <christian.foerster@ort-online.net>
2021-04-16 22:12:48 +0200

    Added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Rigidbody2D player;
+    public Animator animator;
+    public float movementSpeed = 2f;
+    private Vector2 input;
+    
     void Update()
     {
-        
+        this.input.x = Input.GetAxisRaw("Horizontal");
+        this.input.y = Input.GetAxisRaw("Vertical");
+
+        this.animator.SetFloat("Horizontal", this.input.x);
+        this.animator.SetFloat("Vertical", this.input.y);
+        this.animator.SetFloat("Speed", this.input.sqrMagnitude);
     }
+    
+    void FixedUpdate()
+    {
+        this.player.MovePosition(this.player.position + (this.input * this.movementSpeed * Time.fixedDeltaTime));
+    }
+    
 }
